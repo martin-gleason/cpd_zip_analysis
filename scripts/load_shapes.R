a48f690126c72a4d5b9427fbb4960ddc8482c9f0
Martin Gleason <officergleason@gmail.com>
2020-01-29 10:08:47 -0600

    add index.html

--- scripts/load_shapes.R
+++ scripts/load_shapes.R
@@ -7,14 +7,12 @@ library(here)
 library(leaflet)
 
 cpd <- here("shapefiles/Boundaries - Police Districts (current).geojson")
-zips <- here("shapefiles/Boundaries - ZIP Codes.geojson")
 zips_cook <- here("shapefiles/shp_sf")
 
 cpd_sf <- st_read(cpd) %>% st_transform("+proj=longlat +datum=WGS84")
-zips_sf <- st_read(zips) %>% st_transform("+proj=longlat +datum=WGS84")
 cook_shp <- st_read(zips_cook) %>% st_transform("+proj=longlat +datum=WGS84")
 
-geo_json_zip <- st_read("/Users/marty/Downloads/tl_2013_17031_addrfn")
+
 
 CookCounty <- leaflet() %>%
   setView(lat = 41.8781, lng = -87.6298, zoom = 10) %>%
